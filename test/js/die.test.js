f4c3238ad64c312d23c0225a299092ad15d460b8
Raymond Greeley <raymond.greeley.dev@gmail.com>
2023-09-22 21:31:00 -0400

    WIP provide tests around SummedRoll

--- test/js/die.test.js
+++ test/js/die.test.js
@@ -13,7 +13,50 @@
 // limitations under the License.
 
 import '../../src/js/model/die'
+import { Die } from '../../src/js/model/die'
 
-test('Test die class', () => {
-  throw new Error("Method not implemented.");
+
+
+let die;
+let times6;
+
+beforeEach(() => {
+  die = new Die(6)
+  times6 = die.rollMultiple(6)
 })
+
+test('The die has 6 sides', () => {
+  let sides = die.sides
+  expect(sides).toEqual(6)
+})
+
+test('The die rolls a number between 1 and 6', () => {
+  let roll1 = die.roll()
+  expect(roll1).toBeGreaterThanOrEqual(1)
+  expect(roll1).toBeLessThanOrEqual(6)
+})
+
+test('rollMultiple should be an array', () => {
+  expect(Array.isArray(times6)).toBe(true)
+})
+
+test('The die can be rolled multiple times', () => {
+  let times7 = die.rollMultiple(7)
+  expect(times6).toHaveLength(6)
+  expect(times7).toHaveLength(7)
+})
+
+test('The rolls should all be numbers', () => {
+  times6.forEach(number => {
+    expect(typeof number).toBe("number")
+  });
+})
+
+test('Each roll should be <= 6 or >= 1', () => {
+  times6.forEach(number => {
+    expect(number).toBeLessThanOrEqual(6)
+    expect(number).toBeGreaterThanOrEqual(1)
+  })
+})
+
+
